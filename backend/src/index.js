25ece513ecee2af52ac662d05d114236e199ced3
Mariany Ferreira <myf@certi.org.br>
2020-07-20 00:11:15 -0300

    fix: apply linter recommendations and remove eslint dev dependency

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const Koa = require('koa');
+
 const app = new Koa();
 
-app.use(async ctx => {
+app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
 app.listen(3100);
-
 
 module.exports = app;
